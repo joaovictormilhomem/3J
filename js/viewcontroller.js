2429e76ddeff0b93ccfd53e2cb812d947307ac7a
João Victor Milomem <joaovictormilho@gmail.com>
2021-05-31 10:02:29 -0300

    paginação e estilo basico dos commits

---
+++ js/viewcontroller.js
@@ -0,0 +1,45 @@
+const pageRequests   = document.getElementById('page_requests');
+const pageCustomers  = document.getElementById('page_customers');
+const pageStock      = document.getElementById('page_stock');
+const pageCash       = document.getElementById('page_cash');
+const pageManagement = document.getElementById('page_management');
+const pages          = [pageRequests, pageCustomers, pageStock, pageCash, pageManagement];
+
+const btnRequests   = document.getElementById('btn_requests');
+const btnCustomers  = document.getElementById('btn_customers');
+const btnStock      = document.getElementById('btn_stock');
+const btnCash       = document.getElementById('btn_cash');
+const btnManagement = document.getElementById('btn_management');
+const menuButtons   = [btnRequests, btnCustomers, btnStock, btnCash, btnManagement];
+
+let activePage = 1;
+
+btnRequests.onclick = () => {
+    pages.forEach(page => page.style.display = 'none');
+    pageRequests.style.display = 'block';
+    activePage = 1;
+}
+
+btnCustomers.onclick = () => {
+    pages.forEach(page => page.style.display = 'none');
+    pageCustomers.style.display = 'block';
+    activePage = 2;
+}
+
+btnStock.onclick = () => {
+    pages.forEach(page => page.style.display = 'none');
+    pageStock.style.display = 'block';
+    activePage = 3;
+}
+
+btnCash.onclick = () => {
+    pages.forEach(page => page.style.display = 'none');
+    pageCash.style.display = 'block';
+    activePage = 4;
+}
+
+btnManagement.onclick = () => {
+    pages.forEach(page => page.style.display = 'none');
+    pageManagement.style.display = 'block';
+    activePage = 5;
+}
